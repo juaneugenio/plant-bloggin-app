4cd579d118d27678b0724b5fba1aa5aac88136b7
juan eugenio <leuviah77@gmail.com>
2022-12-01 11:53:04 +0100

    Added routes folder, indexroutes, postsroutes and all works fine

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@
 require("dotenv/config");
 require("./db");
 const express = require("express");
+const allRoutes = require("./routes/index.routes");
 
 const app = express();
 
@@ -11,8 +12,8 @@ require("./config")(app);
 
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
-// const allRoutes = require("./routes");
-// app.use("/api", allRoutes);
+
+app.use("/api", allRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
