39d1463166e9a4436da2888becaff8df9ec69ddf
juan eugenio <leuviah77@gmail.com>
2022-12-07 16:30:34 +0100

    client: UI navbar done

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+/** @format */
+import "./navbar.css";
+const Navbar = () => {
+	return (
+		<>
+			<div className="top">
+				<div className="topLeft">
+					<i className="material-symbols-outlined">grass</i>
+					<p className="textLogo">Bl-anty</p>
+				</div>
+				<div className="topCenter">
+					<ul className="center-links">
+						<li>HOME</li>
+						<li>ABOUT</li>
+						<li>CREATE</li>
+						<li>LOGOUT</li>
+					</ul>
+				</div>
+				<div className="topRight">
+					<img
+						src="https://cdn.dribbble.com/users/1577045/screenshots/4914645/media/5146d1dbf9146c4d12a7249e72065a58.png"
+						alt="Photo Profile"
+						className="profileImg"
+					/>
+					<i className=" searchIcon material-symbols-outlined">search</i>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Navbar;
