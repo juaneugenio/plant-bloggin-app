47cc0d2e2575d9c32f7f59e0197d192347f628ba
juan eugenio <leuviah77@gmail.com>
2022-12-07 15:04:35 +0100

    client: handleSubmit function now works. Server: fixed cors in config/index

--- server/config/index.js
+++ server/config/index.js
@@ -18,16 +18,17 @@ module.exports = (app) => {
 	app.set("trust proxy", 1);
 
 	// controls a very specific header to pass headers from the frontend
-	app.use(
-		cors({
-			credentials: true,
-			origin: process.env.ORIGIN || "http://localhost:5000",
-		}),
-	);
+	// app.use(
+	// 	cors({
+	// 		credentials: true,
+	// 		origin: process.env.ORIGIN || "http://localhost:5000",
+	// 	}),
+	// );
 
 	// In development environment the app logs
 	// app.use(logger("dev"));
 	// To have access to `body` property in the request
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: false }));
+	app.use(cors());
 };
