f53d6d537a12e504cfd24e3236b14d867b1192d1
juan eugenio <leuviah77@gmail.com>
2022-12-07 18:07:20 +0100

    client: added Home page and header component. Header styled, but not ready yet

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+/** @format */
+import Header from "../../components/header/header";
+const Home = () => {
+	return (
+		<>
+			<Header />
+			<p>Home page</p>
+		</>
+	);
+};
+
+export default Home;
