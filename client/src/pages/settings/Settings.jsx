5a3c51663b675d4ef3460c777629826e747e6d13
juan eugenio <leuviah77@gmail.com>
2022-12-09 16:54:28 +0100

    client: settings Profile page created and some styles (still working).

---
+++ client/src/pages/settings/Settings.jsx
@@ -0,0 +1,41 @@
+/** @format */
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./settings.css";
+
+const Settings = () => {
+	return (
+		<div className="settigns">
+			<div className="settingsWrapper">
+				<div className="settingsTitle">
+					<span className="settingsTitleUpdate">Update Your Account</span>
+					<span className="settingsTitleDelete">Delete Account</span>
+				</div>
+				<form className="settingsForm">
+					<label>Profile Picture</label>
+					<div className="settingsPP">
+						<img
+							src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+							alt=""
+						/>
+						<label htmlFor="fileInput">
+							<span className="settingsPPIcon material-symbols-outlined">upload</span>
+						</label>
+						<input id="fileInput" type="file" style={{ display: "none" }} className="settingsPPInput" />
+					</div>
+					<label>Username</label>
+					<input type="text" placeholder="Joan" name="name" />
+					<label>Email</label>
+					<input type="email" placeholder="joan@gmail.com" name="email" />
+					<label>Password</label>
+					<input type="password" placeholder="Password" name="password" />
+					<button className="settingsSubmitButton" type="submit">
+						Update
+					</button>
+				</form>
+			</div>
+			<Sidebar />
+		</div>
+	);
+};
+export default Settings;
+// video 1:12:00 https://www.youtube.com/watch?v=tlTdbc5byAs
