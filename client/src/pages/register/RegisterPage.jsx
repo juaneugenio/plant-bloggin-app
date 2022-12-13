a16604e87eb59808abe0ce9a040aa94756fa2181
juan eugenio <leuviah77@gmail.com>
2022-12-13 20:47:22 +0100

    client: intalled react router dom package.

--- client/src/pages/register/RegisterPage.jsx
+++ client/src/pages/register/RegisterPage.jsx
@@ -10,7 +10,7 @@ const RegisterPage = () => {
 				<label>E-mail</label>
 				<input type="text" className="registerInput" placeholder="Enter your Email..." />
 				<label>Password</label>
-				<input type="password" className="registerInput" placeholder="Enter your Password..." />
+				<input type="password" className="registerInput" placeholder="A Password for this account..." />
 				<button className="registerBtn">Register</button>
 				<span className="registerInfo">Already an Account? Please, Login.</span>
 				<button className="loginBtn">Login</button>
