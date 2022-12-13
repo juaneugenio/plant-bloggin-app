45c294c4ef2c4c53450a2c7595af4efd45116164
juan eugenio <leuviah77@gmail.com>
2022-12-13 15:26:26 +0100

    client: registerPage created and style finished

---
+++ client/src/pages/register/RegisterPage.jsx
@@ -0,0 +1,22 @@
+/** @format */
+import "./registerPage.css";
+const RegisterPage = () => {
+	return (
+		<div className="registerContainer">
+			<span className="registerTitle">Register</span>
+			<form className="registerForm">
+				<label>Username</label>
+				<input type="text" className="registerInput" placeholder="Enter your Username..." />
+				<label>E-mail</label>
+				<input type="text" className="registerInput" placeholder="Enter your Email..." />
+				<label>Password</label>
+				<input type="password" className="registerInput" placeholder="Enter your Password..." />
+				<button className="registerBtn">Register</button>
+				<span className="registerInfo">Already an Account? Please, Login.</span>
+				<button className="loginBtn">Login</button>
+			</form>
+		</div>
+	);
+};
+
+export default RegisterPage;
