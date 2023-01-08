7e0b3c5fc0dd0b53699050ca3f28d59067d63895
juan eugenio <leuviah77@gmail.com>
2023-01-08 20:38:15 +0100

    clien: styled in black error validation messages. Sever: created moddleware folder with 2 files (Not used yet). In auth.roues.js file, created the route for /session and his functionality added to the login process

--- server/routes/authen.routes.js
+++ server/routes/authen.routes.js
@@ -4,8 +4,21 @@ const router = require("express").Router();
 const bcrypt = require("bcrypt");
 
 const User = require("../models/User.model");
+const Session = require("../models/Session.model");
 const saltRounds = 10;
 
+//SESSION
+router.get("/session", async (req, res, next) => {
+	if (!req.headers.authorization) {
+		return res.json(null);
+	}
+	const accesToken = req.headers.authorization;
+	const session = await Session.findById(accesToken).populate("user");
+	!session && res.status(400).json({ errorMessage: "⚠️ Session does not exist" });
+	console.log("%c session ▶︎ ", "font-size:13px; background:#993441; color:#ffb8b1;", session);
+
+	res.status(200).json(session);
+});
 //REGISTER
 router.post("/register", async (req, res) => {
 	const { username, password, email } = req.body;
@@ -48,26 +61,26 @@ router.post("/login", async (req, res) => {
 	const { email, password } = req.body;
 	const emailRegex = /^\S+@\S+\.\S+$/;
 	if (!emailRegex.test(email)) {
-		return res.status(400).json({ errorMessage: "Please provide a valid email." });
+		return res.status(400).json({ errorMessage: "⚠️ Please provide a valid email." });
 	}
 
 	if (!password || password.length < 6) {
 		return res.status(400).json({
-			errorMessage: "Your password needs to be at least 6 characters long.",
+			errorMessage: "⚠️ Your password needs to be at least 6 characters long.",
 		});
 	}
 	try {
 		const formPassword = req.body.password;
 		const user = await User.findOne({ email });
-		!user && res.status(400).json({ errorMessage: "1Wrong credentials! Please, check again!" });
+		!user && res.status(400).json({ errorMessage: "⚠️ You don't have an Account yet. Please Register" });
 
 		const passValidation = await bcrypt.compare(formPassword, user.password);
+		!passValidation && res.status(400).json({ errorMessage: "⚠️ Wrong credentials! Please, check again!" });
 
-		!passValidation && res.status(400).json({ errorMessage: "2Wrong credentials! Please, check again!" });
+		const userInSession = await Session.create({ user: user._id, createdAt: Date.now() });
+		console.log("%c userInSession ▶︎ ", "font-size:13px; background:#993441; color:#ffb8b1;", userInSession);
 
-		const { password, ...others } = user._doc;
-
-		res.status(200).json(others);
+		res.status(200).json({ user, accessToken: userInSession._id });
 	} catch (err) {
 		console.log("%c err500 ▶︎ ", "font-size:13px; background:#993441; color:#ffb8b1;", err);
 		res.status(500).json({ "Error500:": err.message });
