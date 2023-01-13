a4509543902d306d0c5dda549e42b6e96049aa93
juan eugenio <leuviah77@gmail.com>
2023-01-13 12:09:10 +0100

    client: fixed previous errors. App working. UseEffect add in App file. Passed the user object to the components. Now the writeBologPage can be accessed. Server : finished Login funciton in authen.routes file.

--- server/middlewares/isLoggedOUT.js
+++ server/middlewares/isLoggedOUT.js
@@ -4,6 +4,7 @@ const Session = require("../models/Session.model");
 module.exports = (req, res, next) => {
 	// if an already logged in user tries to access the login page it
 	// redirects the user to the home page
+
 	if (req.headers.authorization && req.headers.authorization !== "null") {
 		return Session.findById(req.headers.authorization).then((session) => {
 			if (session) {
