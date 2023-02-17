f41bf2262d80eeab8c882f06f5fd83a7ffa306c4
juan eugenio <leuviah77@gmail.com>
2023-02-17 18:40:37 +0100

    cleaning uris

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -5,7 +5,6 @@ import * as USER_HELPERS from "../utils/userToken";
 
 // here we are just making our code look more DRY. With every backend call we must deal with errors and success states. The idea of creating these kinds of services is to make our lives easier in the components
 function internalServerError(err) {
-	console.log("%c err ▶︎ ", "font-size:13px; background:#993441; color:#ffb8b1;", err.message, err);
 	if (err.response && err.response.data && err.response.data.errorMessage) {
 		return {
 			status: false,
