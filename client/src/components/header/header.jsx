0cb8feb1e4472921808e85a0a1e302d61b96e86f
juan eugenio <leuviah77@gmail.com>
2023-03-10 10:40:53 +0100

    client-navbar--finished navbar style. Styled the hero component too and finished.

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -2,19 +2,16 @@
 import "./header.css";
 const header = () => {
 	return (
-		<div className="header">
-			<div className="headerTitles">
-				<span className="headerTitleTitle">My Bl.anty</span>
-				<p className="headerTitleText">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quam laboriosam beatae, hic perferendis veniam
-					id exercitationem.
-				</p>
+		<div className="header-container">
+			<div className="txt-container">
+				<h1 className="txt-title">My Bl.anty</h1>
+				<p className="txt-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
 			</div>
-			<img
-				className="headerImg"
+			{/* <img
+				className="img-bg"
 				src="https://c1.wallpaperflare.com/preview/161/509/211/flower-plant-orchid-nature.jpg"
 				alt="indoor plant"
-			/>
+			/> */}
 		</div>
 	);
 };
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,6 +2,8 @@
 
 import "./navbar.css";
 import * as PATH from "../../utils/paths";
+import defaulUserImg from "../../assets/default-user.webp";
+
 import { Link } from "react-router-dom";
 
 const Navbar = ({ user, handleLogOut }) => {
@@ -29,19 +31,15 @@ const Navbar = ({ user, handleLogOut }) => {
 			<div className="links-right">
 				{user ? (
 					<>
-						<Link to="#" className="link" onClick={handleLogOut}>
-							{user && "LOGOUT"}
-						</Link>
 						<Link to={PATH.TO__USER_PROFILE_PAGE}>
 							<img
-								src={
-									user.profileImage
-										? user.profileImage
-										: "https://imgs.search.brave.com/HAltpxU-sFVODYlpzIneugquzb8EAWr4WmbK6DVZnw4/rs:fit:512:512:1/g:ce/aHR0cHM6Ly9pczIt/c3NsLm16c3RhdGlj/LmNvbS9pbWFnZS90/aHVtYi9QdXJwbGUx/MjMvdjQvZjgvNDMv/ZDAvZjg0M2QwNWMt/MWIxZi04NGY4LWEz/YmQtY2E5YmFjZjA0/MzYzL3NvdXJjZS81/MTJ4NTEyYmIuanBn"
-								}
+								src={user.profileImage ? user.profileImage : "defaulUserImg"}
 								alt="Photo Profile"
 								className="profileImg"
 							/>
+						</Link>
+						<Link to="#" className="link" onClick={handleLogOut}>
+							{user && "LOGOUT"}
 						</Link>
 					</>
 				) : (
